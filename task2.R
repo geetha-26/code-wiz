d1333e6a10673d259e10883c23f6de6cd7384381
Geetha <87968650+geetha-26@users.noreply.github.com>
2021-11-21 15:02:40 +0530

    Add files via upload
    This is project is conducted through R programing.
    The following code consists of how to predict the optimum no. of clusters & representing it visually of IRIS dataset. (Task 2)
    This project is undertaken as a part of GRIP-NOVEMBER '21 of The Sparks Foundation Internship program for Data science and Business analytics.

---
+++ task2.R
@@ -0,0 +1,37 @@
+data=read.csv(file=choose.files(),header=T,sep=",")
+data
+## Decision Tree for Iris dataset
+library(rpart)
+library(rpart.plot)
+
+v <- iris$Species
+
+table(v)
+set.seed(522)
+
+# runif function returns a uniform distribution which can be further conditionally split into 75-25 ratio
+iris[, 'train'] <- ifelse(runif(nrow(iris)) < 0.75, 1, 0)
+
+trainSet <- iris[iris$train == 1,]
+testSet <- iris[iris$train == 0, ]
+
+trainColNum <- grep('train', names(trainSet))
+
+trainSet <- trainSet[, -trainColNum]
+testSet <- testSet[, -trainColNum]
+
+treeFit <- rpart(Species~.,data=trainSet,method = 'class')
+print(treeFit)
+rpart.plot(treeFit, box.col=c("red", "green"))
+
+Prediction1 <- predict(treeFit,newdata=testSet[-5],type = 'class')
+## Pruning the decision tree
+printcp(treeFit)
+opt  <-  which.min(treeFit$cptable[,'xerror'])
+
+cp <-  treeFit$cptable[opt, 'CP']
+pruned_model <-  prune(treeFit,cp)
+rpart.plot(pruned_model, box.col=c("red", "green"))
+rpart_pruned_predict <- predict(pruned_model, newdata=testSet[-5],type = 'class')
+mn2 <- mean(rpart_pruned_predict==testSet$Species)
+mn2
